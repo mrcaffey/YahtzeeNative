463646cc26fabc909f1911ae9a878e51379d3e45
Micah Caffey <mrcaffey@gmail.com>
2018-10-19 11:53:56 -0600

    add login page

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, AsyncStorage } from 'react-native';
 import { AppLoading } from 'expo'
+import { NativeRouter, Route, Switch } from 'react-router-native';
+import { initMiddleware } from 'devise-axios';
 import { Provider } from 'react-redux'
 import store from './store'
+import FetchUser from './components/FetchUser';
+import Auth from './components/Auth';
 
-export default class App extends React.Component {
-  state = { isReady: false }
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    initMiddleware({ storage: AsyncStorage })
+    this.state = { isReady: false }
+  }
 
   getReady = () => {
     return new Promise(resolve => setTimeout(() => resolve("Done"), 3000))
@@ -21,21 +29,20 @@ export default class App extends React.Component {
       />
     )
     return (
-      <Provider storre = {store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automagically reload</Text>
-      </View>
+      <Provider store = {store}>
+        <FetchUser>
+          <NativeRouter>
+            <Switch>
+    <Route exact path="/" render={ props => <Auth {...props} type="Login" /> } />
+              <Route exact path="/login" render={ props => <Auth {...props} type="Login" /> } />
+    <Route exact path="/register" render={ props => <Auth {...props} type="Register" /> } />
+            </Switch>
+          </NativeRouter>
+        </FetchUser>
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default App
