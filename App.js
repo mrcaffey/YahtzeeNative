9995e596469fb958014b8544506851e519d201f0
Micah Caffey <mrcaffey@gmail.com>
2018-10-19 12:15:03 -0600

    yahtzee.js

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import store from './store'
 import FetchUser from './components/FetchUser';
 import Auth from './components/Auth';
+import Yahtzee from './components/Yahtzee';
+import ProtectedRoute from './ProtectedRoute'
 
 class App extends React.Component {
   constructor(props) {
@@ -36,6 +38,7 @@ class App extends React.Component {
     <Route exact path="/" render={ props => <Auth {...props} type="Login" /> } />
               <Route exact path="/login" render={ props => <Auth {...props} type="Login" /> } />
     <Route exact path="/register" render={ props => <Auth {...props} type="Register" /> } />
+            <ProtectedRoute exact path ="/" component={Yahtzee} />
             </Switch>
           </NativeRouter>
         </FetchUser>
